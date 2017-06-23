cd25c619482dbf99ca7513d355aedbe96d88e400
Ben Sams <bensams127@gmail.com>
2017-06-23 08:10:05 -0500

    Initial Commit

--- HighRoller WatchKit Extension/InterfaceController.swift
+++ HighRoller WatchKit Extension/InterfaceController.swift
@@ -8,14 +8,26 @@
 
 import WatchKit
 import Foundation
+import UIKit
+
 
 
 class InterfaceController: WKInterfaceController {
 
+    
+    @IBOutlet var leftWatchDice: WKInterfaceImage!
+    
+    @IBOutlet var rightWatchDice: WKInterfaceImage!
+    
+    var diceRoll1: Int = 1
+    var diceRoll2: Int = 1
+    
+    var diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+       
     }
     
     override func willActivate() {
@@ -27,5 +39,49 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+
+    @IBAction func rollButtonPushed() {
+        
+        updateDiceImages()
+        
+    }
+    func updateDiceImages() {
+        
+        diceRoll1 = Int(arc4random_uniform(6))
+        diceRoll2 = Int(arc4random_uniform(6))
+        
+       
+        if diceRoll1 == 0 {
+            leftWatchDice.setImage(#imageLiteral(resourceName: "dice1watch"))
+        } else if diceRoll1 == 1 {
+            leftWatchDice.setImage(#imageLiteral(resourceName: "dice2watch"))
+        } else if diceRoll1 == 2 {
+            leftWatchDice.setImage(#imageLiteral(resourceName: "dice3watch"))
+        } else if diceRoll1 == 3 {
+            leftWatchDice.setImage(#imageLiteral(resourceName: "dice4watch"))
+        } else if diceRoll1 == 4 {
+            leftWatchDice.setImage(#imageLiteral(resourceName: "dice5watch"))
+        } else if diceRoll1 == 5 {
+            leftWatchDice.setImage(#imageLiteral(resourceName: "dice6watch"))
+        } else {
+            return
+        }
+    
+    if diceRoll2 == 0 {
+    rightWatchDice.setImage(#imageLiteral(resourceName: "dice1watch"))
+    } else if diceRoll2 == 1 {
+    rightWatchDice.setImage(#imageLiteral(resourceName: "dice2watch"))
+    } else if diceRoll2 == 2 {
+    rightWatchDice.setImage(#imageLiteral(resourceName: "dice3watch"))
+    } else if diceRoll2 == 3 {
+    rightWatchDice.setImage(#imageLiteral(resourceName: "dice4watch"))
+    } else if diceRoll2 == 4 {
+    rightWatchDice.setImage(#imageLiteral(resourceName: "dice5watch"))
+    } else if diceRoll2 == 5 {
+    rightWatchDice.setImage(#imageLiteral(resourceName: "dice6watch"))
+    } else {
+    return
+    }
+}
 
 }
---
+++ ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  Junetest17
+//
+//  Created by Sams on 6/2/17.
+//  Copyright © 2017 Sams. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import GoogleMobileAds
+
+class ViewController: UIViewController, AVAudioPlayerDelegate, GADBannerViewDelegate {
+    
+    @IBOutlet weak var myBanner: GADBannerView!
+    var audioPlayer : AVAudioPlayer!
+    
+    var diceRoll1: Int = 1
+    var diceRoll2: Int = 1
+    
+    var diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+ //   let soundArray = ["diceroll", "snakeeyes"]
+    
+    @IBOutlet weak var diceImageLeft: UIImageView!
+    @IBOutlet weak var diceImageRight: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        //test device
+        let request = GADRequest()
+        request.testDevices = [kGADSimulatorID]
+        
+        //setup Ad
+        myBanner.adUnitID = "ca-app-pub-1854353963587972/4221094047"
+        
+        myBanner.rootViewController = self
+        myBanner.delegate = self
+        
+        myBanner.load(request)
+        
+        updateDiceImages()
+        
+    }
+    
+    @IBAction func rollButton(_ sender: Any) {
+        
+        updateDiceImages()
+        
+        soundQuestion()
+        
+    }
+    
+    func updateDiceImages() {
+        
+        diceRoll1 = Int(arc4random_uniform(6))
+        diceRoll2 = Int(arc4random_uniform(6))
+        
+        diceImageLeft.image = UIImage(named: diceArray[diceRoll1])
+        diceImageRight.image = UIImage(named: diceArray[diceRoll2])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+        soundQuestion()
+    }
+    
+    
+    func playSound(soundFileName : String) {
+        
+            let soundURL1 = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL1!)
+        }
+        catch  {
+            print(error)
+        }
+        
+        audioPlayer.play()
+        
+    }
+    
+    func soundQuestion(){
+        if diceRoll1 == 0 && diceRoll2 == 0 {
+            
+            playSound(soundFileName: "snakeeyes")
+            
+        } else {
+            playSound(soundFileName: "diceroll")
+        }
+        
+    }
+    
+}
+
